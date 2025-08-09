883cd2b9dfd2d0b90de5909cd9c36444e3734d6b
ZAGROX <37931784+zagrox@users.noreply.github.com>
2025-08-09 15:50:27 +0330

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,27 @@ import { defineConfig, loadEnv } from 'vite';
 import { fileURLToPath, URL } from 'url';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': fileURLToPath(new URL('.', import.meta.url))
+  const env = loadEnv(mode, '.', '');
+  return {
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+    },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('.', import.meta.url))
+      }
+    },
+    build: {
+      rollupOptions: {
+        external: ['radash', '@leeoniya/ufuzzy'], // Add any modules causing issues
+        output: {
+          globals: {
+            radash: '_',
+            '@leeoniya/ufuzzy': 'Fuzzy'
+          }
         }
       }
-    };
+    }
+  };
 });
